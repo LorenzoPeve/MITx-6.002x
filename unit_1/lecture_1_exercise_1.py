06b3f4cab74972b99e310129d3c75416337d9484
lpeveHA <lpeve01@gmail.com>
2022-10-23 20:18:13 -0500

    Create lecture_1_exercise_1.py

---
+++ unit_1/lecture_1_exercise_1.py
@@ -0,0 +1,11 @@
+"""
+
+STATEMENT: 
+As a burgler robs a house, she finds the following items:
+    Dirt - Weight: 4, Value: 0
+    Computer - Weight: 10, Value: 30
+    Fork - Weight: 5, Value: 1
+    Problem Set - Weight: 0, Value: -10
+
+"""
+
