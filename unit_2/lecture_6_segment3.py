8d592a2c670afccb34027208590f9bd210ee21ae
lpeveHA <lpeve01@gmail.com>
2022-11-06 11:22:39 -0600

    Update lecture_6_segment3.py

---
+++ unit_2/lecture_6_segment3.py
@@ -0,0 +1,156 @@
+import random, pylab
+import matplotlib.pyplot as plt
+
+#set line width
+pylab.rcParams['lines.linewidth'] = 4
+#set font size for titles 
+pylab.rcParams['axes.titlesize'] = 20
+#set font size for labels on axes
+pylab.rcParams['axes.labelsize'] = 20
+#set size of numbers on x-axis
+pylab.rcParams['xtick.labelsize'] = 16
+#set size of numbers on y-axis
+pylab.rcParams['ytick.labelsize'] = 16
+#set size of ticks on x-axis
+pylab.rcParams['xtick.major.size'] = 7
+#set size of ticks on y-axis
+pylab.rcParams['ytick.major.size'] = 7
+#set size of markers, e.g., circles representing points
+#set numpoints for legend
+pylab.rcParams['legend.numpoints'] = 1
+
+from random_walk_simulator import simWalks
+from models import Field, Location, Drunk, UsualDrunk, ColdDrunk
+
+class styleIterator(object):
+    def __init__(self, styles):
+        self.index = 0
+        self.styles = styles
+
+    def nextStyle(self):
+        result = self.styles[self.index]
+        if self.index == len(self.styles) - 1:
+            self.index = 0
+        else:
+            self.index += 1
+        return result
+    
+def simDrunk(numTrials, dClass, walkLengths):
+    meanDistances = []
+    for numSteps in walkLengths:
+        print('Starting simulation of',
+              numSteps, 'steps')
+        trials = simWalks(numSteps, numTrials, dClass)
+        mean = sum(trials)/len(trials)
+        meanDistances.append(mean)
+    return meanDistances
+
+def simAll(drunkKinds, walkLengths, numTrials):
+    styleChoice = styleIterator(('m-', 'b--', 'g-.'))
+    fig, ax = plt.subplots()
+    for dClass in drunkKinds:
+        curStyle = styleChoice.nextStyle()
+        print('Starting simulation of', dClass.__name__)
+        means = simDrunk(numTrials, dClass, walkLengths)        
+        ax.plot(walkLengths, means, curStyle,
+                   label = dClass.__name__)
+    ax.set_title('Mean Distance from Origin ('
+                + str(numTrials) + ' trials)')
+    ax.set_xlabel('Number of Steps')
+    ax.set_ylabel('Distance from Origin')
+    ax.legend(loc = 'best')
+    plt.show()
+
+random.seed(0)
+numSteps = (10,100,1000,10000)
+simAll((UsualDrunk, ColdDrunk), numSteps, 100)
+
+def getFinalLocs(numSteps, numTrials, dClass):
+    locs = []
+    d = dClass()
+    for t in range(numTrials):
+        f = Field()
+        f.addDrunk(d, Location(0, 0))
+        for s in range(numSteps):
+            f.moveDrunk(d)
+        locs.append(f.getLoc(d))
+    return locs
+
+def plotLocs(drunkKinds, numSteps, numTrials):
+    styleChoice = styleIterator(('k+', 'r^', 'mo'))
+    fig, ax = plt.subplots()
+    for dClass in drunkKinds:
+        locs = getFinalLocs(numSteps, numTrials, dClass)
+        xVals, yVals = [], []
+        for loc in locs:
+            xVals.append(loc.getX())
+            yVals.append(loc.getY())
+        xVals = pylab.array(xVals)
+        yVals = pylab.array(yVals)
+        meanX = sum(abs(xVals))/len(xVals)
+        meanY = sum(abs(yVals))/len(yVals)
+        curStyle = styleChoice.nextStyle()
+        ax.plot(xVals, yVals, curStyle,
+                      label = dClass.__name__ +\
+                      ' mean abs dist = <'
+                      + str(meanX) + ', ' + str(meanY) + '>')
+    ax.set_title('Location at End of Walks ('
+                + str(numSteps) + ' steps)')
+    ax.set_ylim(-1000, 1000)
+    ax.set_xlim(-1000, 1000)
+    ax.set_xlabel('Steps East/West of Origin')
+    ax.set_ylabel('Steps North/South of Origin')
+    ax.legend(loc = 'upper left')
+    plt.show()
+
+random.seed(0)
+plotLocs((UsualDrunk, ColdDrunk), 10000, 1000)
+
+class OddField(Field):
+    def __init__(self, numHoles = 1000,
+                 xRange = 100, yRange = 100):
+        Field.__init__(self)
+        self.wormholes = {}
+        for w in range(numHoles):
+            x = random.randint(-xRange, xRange)
+            y = random.randint(-yRange, yRange)
+            newX = random.randint(-xRange, xRange)
+            newY = random.randint(-yRange, yRange)
+            newLoc = Location(newX, newY)
+            self.wormholes[(x, y)] = newLoc
+
+    def moveDrunk(self, drunk):
+        Field.moveDrunk(self, drunk)
+        x = self.drunks[drunk].getX()
+        y = self.drunks[drunk].getY()
+        if (x, y) in self.wormholes:
+            self.drunks[drunk] = self.wormholes[(x, y)]
+            
+#TraceWalk using oddField          
+def traceWalk(fieldKinds, numSteps):
+    styleChoice = styleIterator(('b+', 'r^', 'ko'))
+    fig, ax = plt.subplots()
+    for fClass in fieldKinds:
+        d = UsualDrunk()
+        f = fClass()
+        f.addDrunk(d, Location(0, 0))
+        locs = []
+        for s in range(numSteps):
+            f.moveDrunk(d)
+            locs.append(f.getLoc(d))
+        xVals, yVals = [], []
+        for loc in locs:
+            xVals.append(loc.getX())
+            yVals.append(loc.getY())
+        curStyle = styleChoice.nextStyle()
+        ax.plot(xVals, yVals, curStyle,
+                   label = fClass.__name__)
+    ax.set_title('Spots Visited on Walk ('
+                + str(numSteps) + ' steps)')
+    ax.set_xlabel('Steps East/West of Origin')
+    ax.set_ylabel('Steps North/South of Origin')
+    ax.legend(loc = 'best')
+    plt.show()
+
+random.seed(0)
+traceWalk((Field, OddField), 500)
