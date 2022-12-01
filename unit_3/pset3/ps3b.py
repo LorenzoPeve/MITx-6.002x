80c903163f469cb3b1cdd157c1d2e4da49188989
lpeveHA <lpeve01@gmail.com>
2022-11-30 19:47:56 -0600

    Passing problem 2

--- unit_3/pset3/ps3b.py
+++ unit_3/pset3/ps3b.py
@@ -144,30 +144,47 @@ class Patient(object):
 
         return self.getTotalPop()
 
-
-def simulationWithoutDrug(numViruses, maxPop, maxBirthProb, clearProb,
-                          numTrials):
+def simulationWithoutDrug(
+        numViruses: int, maxPop: int, maxBirthProb: float, 
+        clearProb: float, numTrials: int):
     """
     Run the simulation and plot the graph for problem 3 (no drugs are used,
-    viruses do not have any drug resistance).    
+    viruses do not have any drug resistance).
+
     For each of numTrials trial, instantiates a patient, runs a simulation
     for 300 timesteps, and plots the average virus population size as a
     function of time.
 
-    numViruses: number of SimpleVirus to create for patient (an integer)
-    maxPop: maximum virus population for patient (an integer)
-    maxBirthProb: Maximum reproduction probability (a float between 0-1)        
-    clearProb: Maximum clearance probability (a float between 0-1)
-    numTrials: number of simulation runs to execute (an integer)
+    Args:
+        numViruses (int): number of SimpleVirus to create for patient.
+        maxPop (int): maximum virus population for patient.
+        maxBirthProb (float): Maximum reproduction probability.   
+        clearProb (float): Maximum clearance probability.
+        numTrials (int): number of simulation runs to execute.
     """
+    T = 300
+    pop_at_time_step = [0 for _ in range(T)]
+    for i in range(numTrials):
 
-    # TODO
+        v = [SimpleVirus(maxBirthProb, clearProb) for _ in range(numViruses)]
+        p = Patient(v, maxPop)
+
+        for t in range(T):
+            pop_at_time_step[t] += p.update()
+
+    average_pop = [i/numTrials for i in pop_at_time_step]
+    return average_pop
+
+def plot_population_over_time(pop: list) -> None:
+    
+    pylab.plot(pop, label = "SimpleVirus")
+    pylab.title("SimpleVirus simulation")
+    pylab.xlabel("Time Steps")
+    pylab.ylabel("Average Virus Population")
+    pylab.legend(loc = "best")
+    pylab.show()
 
 
-
-#
-# PROBLEM 3
-#
 class ResistantVirus(SimpleVirus):
     """
     Representation of a virus which can have drug resistance.
